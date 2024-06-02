1e99a11a7336cb504a3c9507cdf81c9eca8cc6fe
Mel0kaue <kauemf10@gmail.com>
2024-06-02 16:47:45 -0300

    adicionando classe principal

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+import visao.jFrmPrincipal;
+
+/**
+ *
+ * @author kauem
+ */
+public class Principal {
+    public static void main(String[] args) {
+        new jFrmPrincipal().setVisible(true);
+    }
+}
